1c76f008844a5622f7999c1f77f8ab6f75c1e8cd
Liane Hampe <liaham@xmera.de>
2021-05-29 14:34:25 +0200

    Improve tests
    * project types fixtures
    * test code clean up

--- test/create_project_type.rb
+++ test/create_project_type.rb
@@ -22,31 +22,14 @@ module TableCalculation
   ##
   # Provide user login test
   #
-  module AuthenticateUser
-    def log_user(login, password)
-      login_page
-      log_user_in(login, password)
-      assert_equal login, User.find(user_session_id).login
-    end
-
-    module_function
-
-    def login_page
-      User.anonymous
-      get '/login'
-      assert_nil user_session_id
-      assert_response :success
-    end
-
-    def user_session_id
-      session[:user_id]
-    end
-
-    def log_user_in(login, password)
-      post '/login', params: {
-        username: login,
-        password: password
-      }
+  module CreateProjectType
+    def project_type_create_params(associates)
+      { project_type:
+        { name: 'Lore ipsum',
+          description: 'for testing',
+          is_public: 0,
+          default_member_role_id: 3,
+          position: 4 }.merge(associates) }
     end
   end
 end
